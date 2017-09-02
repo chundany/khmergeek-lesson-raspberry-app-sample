a689b8978b8e4adf31a124e34d409e51941f6754
vulture0902 <vulture0902@gmail.com>
2017-09-02 22:12:11 +0700

    tmp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
+
+
 end
